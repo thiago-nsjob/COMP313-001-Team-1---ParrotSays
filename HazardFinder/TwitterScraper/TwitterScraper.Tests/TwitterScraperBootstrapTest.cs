898356d44f2fddaf74c75a961eab287956516b9d
Ydelma Rangel <yrangel.rangel@gmail.com>
2020-02-26 16:55:00 -0500

    Created test for TwitterScraper

--- HazardFinder/TwitterScraper/TwitterScraper.Tests/TwitterScraperBootstrapTest.cs
+++ HazardFinder/TwitterScraper/TwitterScraper.Tests/TwitterScraperBootstrapTest.cs
@@ -2,18 +2,28 @@
 using System.Collections.Generic;
 using System.Text;
 using System.Threading.Tasks;
+using TwitterScraper.Bootstrap;
 using Xunit;
 
 namespace TwitterScraper.Tests
 {
     public class TwitterScraperBootstrapTest
     {
+        //  Revisar ************
+        [Fact]
+        public async Task Scraper_Constructor()
+        {
+            Scraper client =
+                   new Scraper();
+
+            Assert.NotNull(client);
+        }
+
         [Fact]
         public async Task Handle()
         {
             var scraper = new TwitterScraper.Bootstrap.Scraper();
             await scraper.Handler(null);
-
         }
     }
 }
