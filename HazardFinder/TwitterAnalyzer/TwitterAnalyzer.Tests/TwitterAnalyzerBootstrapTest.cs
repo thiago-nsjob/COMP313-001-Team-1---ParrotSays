e86852b0e461684fefe3fea5ddd401520c0f908e
Andrea de la Isla Portilla <adelaisl@my.centennialcollege.ca>
2020-02-26 16:54:04 -0500

    Partial Twitter Analyzer Test

---
+++ HazardFinder/TwitterAnalyzer/TwitterAnalyzer.Tests/TwitterAnalyzerBootstrapTest.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TwitterAnalyzer.Tests
+{
+    class TwitterAnalyzerBootstrapTest
+    {
+    }
+}
---
+++ HazardFinder/TwitterAnalyzer/TwitterAnalyzer.Tests/TwitterAnalyzerInfraTest.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using TwitterAnalyzer.Infra.ParrotSays;
+using Xunit;
+
+namespace TwitterAnalyzer.Tests
+{
+   public class TwitterAnalyzerInfraTest
+    {
+        [Fact]
+        public async Task ParrotSaysClient_Constructor()
+        {
+            ParrotSaysClient client =
+                   new ParrotSaysClient("https://www.centennialcollege.ca/studenthub/");
+            Assert.NotNull(client);
+        }
+
+    }
+}
