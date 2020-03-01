99f045a4de66e97bc2a77ffc14dccbbb3ea8571c
Eduardo Santana <eduardoamparos@gmail.com>
2020-02-29 23:20:43 -0500

    Adding new configuration to dev

--- Legacy/WebServer/ParrotSays_API/src/main/java/com/spring/SwaggerConfig.java
+++ Legacy/WebServer/ParrotSays_API/src/main/java/com/spring/SwaggerConfig.java
@@ -1,39 +1,102 @@
 package com.spring;
 
-import springfox.documentation.builders.PathSelectors;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.autoconfigure.data.web.SpringDataWebProperties.Pageable;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import springfox.documentation.builders.RequestHandlerSelectors;
+import org.springframework.context.annotation.Import;
+import org.springframework.http.ResponseEntity;
+import static springfox.documentation.builders.PathSelectors.regex;
+import com.google.common.collect.Lists;
+
 import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.ApiKey;
+import springfox.documentation.service.AuthorizationScope;
 import springfox.documentation.service.Contact;
+import springfox.documentation.service.SecurityReference;
+import springfox.documentation.service.VendorExtension;
+import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spi.service.contexts.SecurityContext;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
-import static springfox.documentation.builders.PathSelectors.regex;
 
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
 // to access to swagger please go to: http://localhost:8167/swagger-ui.html
 @Configuration
 @EnableSwagger2
+@Import(springfox.bean.validators.configuration.BeanValidatorPluginsConfiguration.class)
 public class SwaggerConfig {
-    @Bean
-    public Docket productApi() {
-         return new Docket(DocumentationType.SWAGGER_2)  
-          .select()                                  
-          .apis(RequestHandlerSelectors.basePackage("com.spring.controllers"))         
-          .paths(PathSelectors.any())                          
-          .build()
-          .apiInfo(metaData());    
-    }
 
-    private ApiInfo metaData() {
-        ApiInfo apiInfo = new ApiInfo(
-                "Parrot Says REST API",
-                "Parrot Says REST API",
-                "1.0",
-                "Terms of service",
-                new Contact("Parrot Says", "", ""),
-               "Apache License Version 2.0",
-                "https://www.apache.org/licenses/LICENSE-2.0");
-        return apiInfo;
-    }
+	 public static final String AUTHORIZATION_HEADER = "Authorization";
+	    public static final String DEFAULT_INCLUDE_PATTERN = "/api/.*";
+	    private final Logger log = LoggerFactory.getLogger(SwaggerConfig.class);
+
+	    @SuppressWarnings("rawtypes")
+		@Bean
+	    public Docket swaggerSpringfoxDocket() {
+	        log.debug("Starting Swagger");
+	        Contact contact = new Contact(
+	            "Matyas Albert-Nagy",
+	            "https://justrocket.de",
+	            "matyas@justrocket.de");
+
+	        List<VendorExtension> vext = new ArrayList<>();
+	        ApiInfo apiInfo = new ApiInfo(
+	            "Backend API",
+	            "This is the best stuff since sliced bread - API",
+	            "6.6.6",
+	            "https://justrocket.de",
+	            contact,
+	            "MIT",
+	            "https://justrocket.de",
+	            vext);
+
+	        Docket docket = new Docket(DocumentationType.SWAGGER_2)
+	            .apiInfo(apiInfo)
+	            .pathMapping("/")
+	            .apiInfo(ApiInfo.DEFAULT)
+	            .forCodeGeneration(true)
+	            .genericModelSubstitutes(ResponseEntity.class)
+	            .ignoredParameterTypes(Pageable.class)
+	            .ignoredParameterTypes(java.sql.Date.class)
+	            .directModelSubstitute(java.time.LocalDate.class, java.sql.Date.class)
+	            .directModelSubstitute(java.time.ZonedDateTime.class, Date.class)
+	            .directModelSubstitute(java.time.LocalDateTime.class, Date.class)
+	            .securityContexts(Lists.newArrayList(securityContext()))
+	            .securitySchemes(Lists.newArrayList(apiKey()))
+	            .useDefaultResponseMessages(false);
+
+	        docket = docket.select()
+	            .paths(regex(DEFAULT_INCLUDE_PATTERN))
+	            .build();
+	    
+	        log.debug("Started Swagger in {} ms");
+	        return docket;
+	    }
+
+
+	    private ApiKey apiKey() {
+	        return new ApiKey("JWT", AUTHORIZATION_HEADER, "header");
+	    }
+
+	    private SecurityContext securityContext() {
+	        return SecurityContext.builder()
+	            .securityReferences(defaultAuth())
+	            .forPaths(PathSelectors.regex(DEFAULT_INCLUDE_PATTERN))
+	            .build();
+	    }
+
+	    List<SecurityReference> defaultAuth() {
+	        AuthorizationScope authorizationScope
+	            = new AuthorizationScope("global", "accessEverything");
+	        AuthorizationScope[] authorizationScopes = new AuthorizationScope[1];
+	        authorizationScopes[0] = authorizationScope;
+	        return Lists.newArrayList(
+	            new SecurityReference("JWT", authorizationScopes));
+	    }
+    
 }
--- Legacy/WebServer/ParrotSays_API/src/main/java/com/spring/security/SecurityConfig.java
+++ Legacy/WebServer/ParrotSays_API/src/main/java/com/spring/security/SecurityConfig.java
@@ -12,6 +12,7 @@ import org.springframework.security.config.BeanIds;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -60,6 +61,17 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
 
         auth.userDetailsService(userDetailsService).passwordEncoder(encoder);
+    }
+
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring().antMatchers("/v2/api-docs", 
+        		"/configuration/**", 
+        		"/swagger-resources/**",  
+        		"/swagger-ui.html", 
+        		"/webjars/**", 
+        		"/api-docs/**", 
+        		"/webjars/springfox-swagger-ui/css/typography.css");
     }
 
 }
