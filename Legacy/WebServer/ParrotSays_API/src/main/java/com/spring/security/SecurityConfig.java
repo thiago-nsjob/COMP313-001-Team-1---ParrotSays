1f04dee5a4fa574a7f4df9356f4978c00db12823
JViniX <47263194+JViniX@users.noreply.github.com>
2020-02-29 21:10:23 -0500

    Mobile App new login route + Lists performance improvement
    Lists Performance Improvement:
    - getAll and getByStatus without image

--- Legacy/WebServer/ParrotSays_API/src/main/java/com/spring/security/SecurityConfig.java
+++ Legacy/WebServer/ParrotSays_API/src/main/java/com/spring/security/SecurityConfig.java
@@ -42,6 +42,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
         http
                 .authorizeRequests()
+                .antMatchers("/**").permitAll()
                 .antMatchers(HttpMethod.POST, "/api/users/login").permitAll()
                 .antMatchers(HttpMethod.POST, "/api/reports/addreport/**").permitAll()
                 .anyRequest().authenticated()
