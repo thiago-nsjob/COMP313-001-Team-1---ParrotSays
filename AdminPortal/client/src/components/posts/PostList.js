f07cccab37c845a7acca4e7e1f639e107dd60838
thiago do nascimento fontes silva <thiago.nsjob@hotmail.com>
2020-04-11 01:26:28 -0400

    post list

--- AdminPortal/client/src/components/posts/PostList.js
+++ AdminPortal/client/src/components/posts/PostList.js
@@ -1,27 +1,47 @@
-import React, { useState, useEffect } from 'react';
-import { PostAPI } from './api-post';
+import React, { useState, useEffect } from "react";
+import { PostAPI } from "./api-post";
+import Card from "react-bootstrap/Card";
+import Button from "react-bootstrap/Button";
+export const PostList = () => {
+  const [data, setData] = useState([]);
+  const [refresh, setRefresh] = useState(true);
 
-
-export const PostList =() =>{
+  const fetch = async () => {
+    if(refresh){
+        let res = await PostAPI.getAllPosts();
+        console.log(res);
+        setData(res);
+        setRefresh(false);
+    }
     
-    useEffect(() => {
-         PostAPI.getAllPosts();
-      }, []); 
+  };
 
-    const [data, setData] = useState([]);
-  
-      return (
-    
-          
-      <div>
+  useEffect(() => {
+    fetch();
+  }, [data]);
 
-      </div>
-    );
-
-    
-}
-
-
-
+  return (
+    <div className="container-fluid mt-5" style={{width:"70em", marginTop:"10em"}}>
+      {data.map((post, idx) => {
+          return (
+            <Card
+              key={idx}
+              className="m-3"
+               
+            >
+              <Card.Header style={{backgroundColor:"rgb(216, 27, 96)" }}> Id: ${post._id}</Card.Header>
+              <Card.Body >
+                <Card.Title> Card Title </Card.Title>
+                <Card.Text>
+                  {post.text}
+                </Card.Text>
+                  <Button variant="danger">Create Report</Button>  
+              </Card.Body>
+            </Card>
+          );
+        })}
+    </div>
+  );
+};
 
 export default PostList;
--- AdminPortal/client/src/components/posts/api-post.js
+++ AdminPortal/client/src/components/posts/api-post.js
@@ -4,9 +4,8 @@ const POST_API_ROOT = process.env.REACT_APP_POSTS_API;
 
 export const PostAPI = {
   getAllPosts: async () => {
-    let result = await axios.get(POST_API_ROOT + "/post", {
-     
-    });
-    return result.data;
+
+    let result = await axios.get(POST_API_ROOT + "/post");
+    return result.data.data;
   },
 };
