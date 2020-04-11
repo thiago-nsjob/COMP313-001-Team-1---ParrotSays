9b0b04fea859bb86b176bef84911d8ccde9e7023
thiago do nascimento fontes silva <thiago.nsjob@hotmail.com>
2020-04-10 23:18:33 -0400

    config bootstrap

--- AdminPortal/client/src/components/posts/PostList.js
+++ AdminPortal/client/src/components/posts/PostList.js
@@ -1,47 +1,21 @@
 import React, { useState, useEffect } from 'react';
+import { PostAPI } from './api-post';
 
-function PostList(){
+
+export const PostList =() =>{
     
+    useEffect(() => {
+         PostAPI.getAllPosts();
+      }, []); 
+
     const [data, setData] = useState([]);
   
       return (
-        <div className="container" style={{marginTop: "125px"}}>
-            <div className="card">
-            <div className="card-header"><h3>List of Posts</h3></div>
-            <div className="card-body">
-            <table className="table table-striped">
-            <thead class="text-white" style={{background:"#D81B60"}}>
-                <tr>
-                    <th>Report Id</th>
-                    <th>Description</th>
-                    <th>Latitude</th>
-                    <th>Longtitude</th>
-                    <th>Time</th>
-                    <th>Status</th>
-                    <th>Action</th>
-                </tr>
-                </thead>
-                <tbody>
-                    {data.map((value) =>      
-                            <tr key={value.reportId}>
-                                <td>{value.reportId}</td>
-                                <td>{value.description}</td>
-                                <td>{value.latitude}</td>
-                                <td>{value.longitude}</td>
-                                <td>{value.dateTimeReport}</td>
-                                <td>{value.statusCode}</td>
-                                <td><a href={'/edit?id='+ value.reportId}><i className="fa fa-edit text-body"></i></a> | <a href={'/delete?id=' + value.reportId}><i className="fa fa-trash text-body"></i></a></td>
-                            </tr>       
-                    )}   
-                </tbody>
-            </table>
-            </div>
-           
-            </div>
+    
           
-        </div>
+      <div>
 
-      
+      </div>
     );
 
     
